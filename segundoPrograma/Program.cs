6252a72775607ccb4472a5bcc97379b38eedc1ac
Mario Soledispa <mariosoledispa2001@gmail.com>
2020-06-28 18:03:22 -0500

    Agregar archivos de proyecto.

---
+++ segundoPrograma/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace segundoPrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
